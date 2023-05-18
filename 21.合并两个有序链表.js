46aaeae664c9e8266f88bcbf8f1fff2173ada78e
rikusrocks <rikusrocks@gmail.com>
2023-05-18 22:16:27 +1000

    leetcode21/ grind75 递归

--- 21.合并两个有序链表.js
+++ 21.合并两个有序链表.js
@@ -19,20 +19,29 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    let prehead = new ListNode(-1)
-    let prev = prehead
-    while(list1 && list2) {
-        if(list1.val <= list2.val){
-            prev.next = list1
-            list1 = list1.next
-        } else {
-            prev.next = list2
-            list2 = list2.next
-        }
-        prev = prev.next
+    // let prehead = new ListNode(-1)
+    // let prev = prehead
+    // while(list1 && list2) {
+    //     if(list1.val <= list2.val){
+    //         prev.next = list1
+    //         list1 = list1.next
+    //     } else {
+    //         prev.next = list2
+    //         list2 = list2.next
+    //     }
+    //     prev = prev.next
+    // }
+    // prev.next = list1 || list2
+    // return prehead.next
+    if(!list1) return list2
+    if(!list2) return list1
+    if(list1.val <= list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
     }
-    prev.next = list1 || list2
-    return prehead.next
 };
 // @lc code=end
 
