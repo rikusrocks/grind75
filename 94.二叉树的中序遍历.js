e37d98e76f1909c27af18e5f46ea37a3f57d2a51
rikusrocks <rikusrocks@gmail.com>
2023-06-08 12:23:37 +1000

    leetcode 94

---
+++ 94.二叉树的中序遍历.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ * @lcpr version=21909
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+
+  //Definition for a binary tree node.
+  function TreeNode(val, left, right) {
+      this.val = (val===undefined ? 0 : val)
+      this.left = (left===undefined ? null : left)
+      this.right = (right===undefined ? null : right)
+ }
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const result = []
+    const reorder = (root) => {
+        if(!root) {
+            return;
+        }
+        reorder(root.left)
+        result.push(root.val)
+        reorder(root.right)
+    }
+    reorder(root)
+    return result
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,null,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+
