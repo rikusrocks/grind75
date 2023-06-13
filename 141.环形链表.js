bf3c38f9d633eea16d73fc1518e5d3da4aff4ff1
rikusrocks <rikusrocks@gmail.com>
2023-06-13 14:28:47 +1000

    leetcode 141

---
+++ 141.环形链表.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=javascript
+ * @lcpr version=21909
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let slow = head
+    let fast = head
+    if(!head) return false
+    while(fast !== null && fast.next !== null) {
+        fast = fast.next.next
+        slow = slow.next
+        if(fast === slow) return true
+    }
+    return false
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,2,0,-4]\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n0\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n-1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1, 2]\n-1\n
+// @lcpr case=end
+
+ */
+
