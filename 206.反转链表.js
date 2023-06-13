de0284f647b73bedd3c42db3379d7d75cfd960c9
rikusrocks <rikusrocks@gmail.com>
2023-06-13 16:08:43 +1000

    leetcode 206 iteration

---
+++ 206.反转链表.js
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ * @lcpr version=21909
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let pre = null
+    let current = head
+    while(current) {
+        let next = current.next
+        current.next = pre
+        pre = current
+        current = next
+    }
+    return pre
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
+
