5962239d00ffa137b01f96c1b288762636048f5f
rikusrocks <rikusrocks@gmail.com>
2023-06-13 14:57:30 +1000

    leetcode 160

---
+++ 160.相交链表.js
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=javascript
+ * @lcpr version=21909
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    let visited = new Set()
+    while(headA) {
+        visited.add(headA)
+        headA = headA.next
+    }
+    while(headB) {
+        if(visited.has(headB)) return headB
+        headB = headB.next
+    }
+
+    return null
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 8\n[4,1,8,4,5]\n[5,6,1,8,4,5]\n2\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 2\n[1,9,1,2,4]\n[3,2,4]\n3\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 0\n[2,6,4]\n[1,5]\n3\n2\n
+// @lcpr case=end
+
+ */
+
