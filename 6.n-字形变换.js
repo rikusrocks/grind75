f448c7921f926674eb8b1e7bcc5cd0c35b616534
rikusrocks <rikusrocks@gmail.com>
2023-06-14 15:31:07 +1000

    add commments

--- 6.n-字形变换.js
+++ 6.n-字形变换.js
@@ -13,6 +13,12 @@
  */
 var convert = function(s, numRows) {
     let map = new Map()
+    /**
+     * i = 0 0 % 6 = 0  6 - 0 = 6               | i = 6 6 % 6 = 0 6 - 6 = 0
+     * i = 1 1 % 6 = 1  6 - 1 = 5        | i = 5 5 % 6 = 5 6 - 5 = 1           
+     * i = 2 2 % 6 = 2  6 - 2 = 4 | i = 4 4 % 6 = 4 6 - 4 = 2
+     * i = 3 3 % 6 = 3
+     */
     for(let i = 0; i < s.length; i ++) {
         let number = 2 * numRows - 2
         const x = i % number
