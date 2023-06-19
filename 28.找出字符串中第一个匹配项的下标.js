4c4c99c7b93cab5a96f4906eada27ef28eee82a0
rikusrocks <rikusrocks@gmail.com>
2023-06-19 18:27:37 +1000

    leetcode 28

---
+++ 28.找出字符串中第一个匹配项的下标.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=javascript
+ * @lcpr version=21909
+ *
+ * [28] 找出字符串中第一个匹配项的下标
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "sadbutsad"\n"sad"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "leetcode"\n"leeto"\n
+// @lcpr case=end
+
+ */
+
