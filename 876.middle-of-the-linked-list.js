0b1b09a1b7ebd2ff0163b9910a6b241e5cf6b768
rikusrocks <rikusrocks@gmail.com>
2023-09-04 16:18:33 +1000

    leetcode876

---
+++ 876.middle-of-the-linked-list.js
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=876 lang=javascript
+ * @lcpr version=21913
+ *
+ * [876] 链表的中间结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let left = head, right = head
+    while(right!== null && right.next !== null) {
+        left = left.next
+        right = right.next.next
+    }
+
+    return left
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4,5,6]\n
+// @lcpr case=end
+
+ */
+
