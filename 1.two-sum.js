f4a8c4822c8fea53bbc282a46aef15662c267404
rikusrocks <rikusrocks@gmail.com>
2024-12-18 17:07:52 +1100

    leetcode 1 map

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -15,29 +15,39 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  let copy = [...nums];
-  nums.sort((a, b) => a - b);
-  for (let index = 0; index < nums.length; index++) {
-    const element = nums[index];
-    let targetNum = target - element;
-    if(targetNum === element) {
-        let tempIndex = copy.indexOf(element)
-        copy[tempIndex] = null;
-        return [tempIndex, copy.indexOf(element)]
+//   let copy = [...nums];
+//   nums.sort((a, b) => a - b);
+//   for (let index = 0; index < nums.length; index++) {
+//     const element = nums[index];
+//     let targetNum = target - element;
+//     if(targetNum === element) {
+//         let tempIndex = copy.indexOf(element)
+//         copy[tempIndex] = null;
+//         return [tempIndex, copy.indexOf(element)]
+//     }
+//     let left = index,
+//       right = nums.length - 1;
+//     while (left <= right) {
+//       let mid = Math.floor((right + left) / 2);
+//       if (nums[mid] === targetNum) {
+//         return [copy.indexOf(element), copy.indexOf(nums[mid])];
+//       } else if (targetNum > nums[mid]) {
+//         left = mid + 1;
+//       } else {
+//         right = mid - 1;
+//       }
+//     }
+//   }
+    let map = new Map()
+    for (let index = 0; index < nums.length; index++) {
+        const element = nums[index];
+        const targerNum = target - element
+        if(map.has(targerNum)) {
+            return [map.get(targerNum), index]
+        } else {
+            map.set(element, index)
+        }
     }
-    let left = index,
-      right = nums.length - 1;
-    while (left <= right) {
-      let mid = Math.floor((right + left) / 2);
-      if (nums[mid] === targetNum) {
-        return [copy.indexOf(element), copy.indexOf(nums[mid])];
-      } else if (targetNum > nums[mid]) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    }
-  }
 };
 
 // @lc code=end
