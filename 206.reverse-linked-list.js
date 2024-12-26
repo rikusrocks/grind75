c4ed8916f4214f988556958b01d4a471a727ab1b
rikusrocks <rikusrocks@gmail.com>
2024-12-26 13:19:20 +1100

    206

---
+++ 206.reverse-linked-list.js
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ * @lcpr version=30204
+ *
+ * [206] 反转链表
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let pre = null
+    let cur = head
+    while(cur) {
+        let next = cur.next
+        cur.next = pre
+        pre = cur
+        cur = next
+    }
+    return pre
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
+
