38c3a1134dadc9560a8d29f0f8ed8e3612d5d56b
rikusrocks <rikusrocks@gmail.com>
2025-01-03 19:37:55 +1100

    236

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.js
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=javascript
+ * @lcpr version=30204
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if(!root) return null
+    // if it is p or q, return the root
+    if(root === p || root === q) return root
+    const left = lowestCommonAncestor(root.left, p, q)
+    const right = lowestCommonAncestor(root.right, p, q)
+    // if it has the root from both side
+    if(left && right){ 
+        return root
+    }
+    // if it only has one side, means they are on the same side
+    if(left === null && right) return right
+    if(right === null && left) return left
+    // if no returns from left and right
+    return null
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,5,1,6,2,0,8,null,null,7,4]\n5\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,5,1,6,2,0,8,null,null,7,4]\n5\n4\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n1\n2\n
+// @lcpr case=end
+
+ */
+
